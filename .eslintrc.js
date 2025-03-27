0b7a5df49d70aa55fe92696b0d57719cc64ae22f
Victor_Greed <25359373-Victor_Greed@users.noreply.gitlab.com>
2025-03-27 13:50:04 +0100

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,13 +21,29 @@ module.exports = {
     }
   ],
   parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
     project: ["./tsconfig.json"]
   },
   plugins: ["@typescript-eslint"],
+  globals: {
+    'bootstrap': 'readonly'
+  },
   rules: {
+    'no-undef': 'off',
+    'no-restricted-globals': 'off',
+    
+    'no-console': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-else-return': 'off',
+    'object-shorthand': 'warn',
+    'no-trailing-spaces': 'warn',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'no-undef': 'warn',
+    'eol-last': ['error', 'always'],
+
     "class-methods-use-this": 0,
     "comma-dangle": ["error", "never"],
     "linebreak-style": 0,
